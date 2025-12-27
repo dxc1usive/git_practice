a3bebd2a7a5cf10dfc88931e2ca1b28787b50cd6
Zhengyao <justinJZ2007@gmail.com>
2025-12-27 16:08:52 -0500

    chore: initial website scaffold

---
+++ README.md
@@ -0,0 +1,8 @@
+# Git Practice Website
+
+A tiny fake website repo to practice real Git workflows:
+- main / develop
+- feature branches
+- hotfix branches
+- commits, merges, push/pull
+- safe undo (revert)
---
+++ js/app.js
@@ -0,0 +1,9 @@
+(function () {
+  const nav = document.getElementById("nav");
+  if (nav) {
+    nav.innerHTML = `
+      <a href="index.html">Home</a>
+      <a href="about.html">About</a>
+    `;
+  }
+})();
