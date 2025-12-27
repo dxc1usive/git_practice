10a3c6f02972f62d11f889e9fd17f6af42307deb
Zhengyao <justinJZ2007@gmail.com>
2025-12-27 16:51:38 -0500

    hotfix: add aria-label to nav

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 (function () {
   const nav = document.getElementById("nav");
+  if (nav) nav.setAttribute("aria-label", "Primary");
   if (nav) {
     nav.innerHTML = `
       <a href="index.html">Home</a>
